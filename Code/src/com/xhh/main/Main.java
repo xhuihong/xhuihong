984e7aa56d5249b20ba12357c4ea0e34cc513298
xhuihong <17390606329@163.com>
2020-01-23 11:32:48 +0800

    使用idea进行第三次修改

--- Code/src/com/xhh/main/Main.java
+++ Code/src/com/xhh/main/Main.java
@@ -5,5 +5,6 @@ public class Main {
 		System.out.println("第二次测试");
 		System.out.println("我是使用idea就行修改");
         System.out.println("我第二次使用idea就行修改");
+		System.out.println("第三次对idea进行修改");
 	}
 }
